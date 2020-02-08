2ec123d51bf66b9c4e7084e60419764f6ad06515
EC2 Default User <ec2-user@ip-172-31-30-125.us-east-2.compute.internal>
2020-02-08 08:45:54 +0000

    Add tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200208083635) do
+ActiveRecord::Schema.define(version: 20200208084130) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
     t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
